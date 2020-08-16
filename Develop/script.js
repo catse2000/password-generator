e6c74e6299d56ac68a256f6a98c72e072a2c0520
catse2000 <catse2000@yahoo.com>
2020-08-16 16:54:44 -0500

    add variables and generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,41 @@
 // Assignment code here
+// #generate is button
+// #password is textarea
+var passwordLowerCase;
+var passwordUpperCase;
+var passwordNumber;
+var passwordSpecial;
+var passwordArray = [];
+var passwordLowerCharacters = ["abcdefghijklmnopqrstuvwxyz"];
+var passwordUpperCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var passwordNumbers = ["0123456789"];
+var passwordSpecials = ["@%!#$^?~&*()_-+={}[]|/:<>"];
 
 
 // Get references to the #generate element
+// looks for first element labeled #generate and assigns it as a value to generateBtn (generateBtn == #generate)
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  
+  
+}
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
+  // looks for first element labeled #password and assign it as a value to passwordText (passwordText == #password)
   var passwordText = document.querySelector("#password");
 
+  
+  
+  
+  // whatever password is will be assigned to #password textarea
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+// performs actions in writePassword when #generate is clicked
 generateBtn.addEventListener("click", writePassword);
